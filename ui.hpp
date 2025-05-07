ff858f0eb6c0bfa70c1ac6d858b92a23d94c7d92
Ian <80492876+TWian1@users.noreply.github.com>
2025-05-07 19:23:10 -0400

    Update ui.hpp

--- ui.hpp
+++ ui.hpp
@@ -15,6 +15,7 @@ private:
 	void updateTexture();
 public:
 	SDL_Rect rect;
+	std::string id;
 	float x, y;
 	int font_size;
 	bool visible = true;
@@ -22,7 +23,7 @@ public:
 	std::string fontPath;
 	std::vector<textBox> textBoxes;
 	SDL_Color textColor, backgroundColor;
-	textBox(SDL_Renderer* renderer, TTF_Font* font, std::string fontPath, float x, float y, std::string text,
+	textBox(SDL_Renderer* renderer, std::string id, TTF_Font* font, std::string fontPath, float x, float y, std::string text,
 		SDL_Color textColor = { 0, 0, 0, 255 }, SDL_Color backgroundColor = { 0, 0, 0, 0 }, int font_size = -1);
 	void setText(const std::string& newText);
 	void Render();
@@ -35,6 +36,7 @@ private:
 	TTF_Font* font;
 public:
 	float x, y, w, h;
+	std::string id;
 	bool hover = false;
 	bool visible = true;
 	bool clickable = true;
@@ -43,8 +45,16 @@ public:
 	SDL_Color default_color, pressed_color, hover_color;
 	std::function<void()> func;
 	std::vector<button> buttons;
-	button(std::function<void()> func, std::string text, TTF_Font* font, float x, float y, SDL_Renderer* renderer, float w = 100.0f, float h = 25.0f, int font_size = -1, 
+	button(std::function<void()> func, SDL_Renderer* renderer, std::string id, std::string text, TTF_Font* font, float x, float y, float w = 100.0f, float h = 25.0f, int font_size = -1,
 		SDL_Color default_color = { 255, 255, 255, 255 }, SDL_Color hover_color = { 200, 200, 200, 255 }, SDL_Color pressed_color = { 100, 100, 100, 255 }, SDL_Color text_color = { 0, 0, 0, 255 });
 	bool click_test(vec2<float> MouseCoords, bool call = true);
 	void Render();
 };
+template <typename T>
+T* getObjectById(std::vector<T>& objects, const std::string& id) {
+	for (auto& obj : objects) {
+		if (obj.id == id) return &obj;
+	}
+	return nullptr;  // Return nullptr if no matching object is found
+}
+void renderUI(std::vector<textBox>& textBoxes, std::vector<button>& buttons);
