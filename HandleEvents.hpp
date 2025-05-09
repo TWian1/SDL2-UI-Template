4fda8eadde1e50799e1ba3be47eae32885a085ea
Ian <80492876+TWian1@users.noreply.github.com>
2025-05-08 23:11:01 -0400

    Update HandleEvents.hpp

--- HandleEvents.hpp
+++ HandleEvents.hpp
@@ -1,10 +1,3 @@
 #pragma once
-#include <SDL.h>
-#include <vector>
-#include "vec.hpp"
 #include "ui.hpp"
-
-
-extern int window_width;
-extern int window_height;
 void handleEvents(bool& gameloop, std::vector<button>& buttons, SDL_Renderer* renderer);
