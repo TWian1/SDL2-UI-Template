1108e1f9d72124ce3c5d736d87dd275f4da50885
Ian <80492876+TWian1@users.noreply.github.com>
2025-05-10 17:16:39 -0400

    Update main.cpp
    Compacted error detection added for new format

--- main.cpp
+++ main.cpp
@@ -20,43 +20,14 @@ void print(int& Generation, textBox* gentext) {
 int main() {
 
 	//Initialize SDL and SDL_TTF and check for Errors
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-		cout << "SDL_Init Error: " << SDL_GetError() << endl;
-		return 1;
-	}
-	if (TTF_Init() != 0) {
-		cout << "TTF_Init Error: " << TTF_GetError() << "\n";
-		SDL_Quit();
-		return 1;
-	}
-
-	// Create Window and check for errors
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) { cout << "SDL_Init Error: " << SDL_GetError() << endl; return 1; }
+	if (TTF_Init() != 0) { cout << "TTF_Init Error: " << TTF_GetError() << "\n"; SDL_Quit(); return 1; }
 	SDL_Window* window = SDL_CreateWindow("help", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1920, 1080, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
-	if (window == nullptr) {
-		cout << "SDL_CreateWindow Error: " << SDL_GetError() << endl;
-		SDL_Quit();
-		return 1;
-	}
-
-	// Create renderer and check for errors
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-	if (renderer == nullptr) {
-		cout << "SDL_CreateRenderer Error: " << SDL_GetError() << endl;
-		SDL_DestroyWindow(window);
-		SDL_Quit();
-		return 1;
-	}
-
-	// Open font and check for errors
+	if (renderer == nullptr) { cout << "SDL_CreateRenderer Error: " << SDL_GetError() << endl; SDL_DestroyWindow(window); SDL_Quit(); return 1; }
 	TTF_Font* font = TTF_OpenFont("OpenSans-ExtraBold.ttf", 24);
-	if (!font) {
-		cout << "TTF_OpenFont Error: " << TTF_GetError() << "\n";
-		SDL_DestroyRenderer(renderer);
-		SDL_DestroyWindow(window);
-		TTF_Quit();
-		SDL_Quit();
-		return 1;
-	}
+	TTF_Font* font2 = TTF_OpenFont("OpenSans.ttf", 24);
+	if (!font2 || !font) { cout << "TTF_OpenFont Error: " << TTF_GetError() << "\n"; SDL_DestroyRenderer(renderer); SDL_DestroyWindow(window); TTF_Quit(); SDL_Quit(); return 1; }
 
 	// Initialize all of the required variables
 	bool gameloop = true;
@@ -65,20 +36,22 @@ int main() {
 	double deltaTime = 0;
 
 	int Generation = 0;
-	ui.text.push_back(textBox(renderer, "FPS_Counter", font, "OpenSans-ExtraBold.ttf", 25, 25, "FPS: 0", {255, 255, 255, 255}, 20));
+	ui.text.push_back(textBox(renderer, "FPS_Counter", font, "OpenSans-ExtraBold.ttf", 25, 25, "FPS: 0", { 255, 255, 255, 255 }, 20));
 	ui.text.push_back(textBox(renderer, "Gen_Text", font, "OpenSans-ExtraBold.ttf", 25, 995, "Generation 0", { 255, 255, 255, 255 }, 50, { 0, 0, 0, 0 }, 15));
-	ui.buttons.push_back(button([&]() { print(Generation, getObjectById(ui.text, "Gen_Text")); }, renderer, "Generation_Button", "TEST", font,
+	ui.buttons.push_back(button([&]() { print(Generation, getObjectById(ui.text, "Gen_Text")); }, renderer, "Generation_Button", "TEST", font, "OpenSans-ExtraBold.ttf",
 		1700, 970, 150.0, 75.0, 45, { 255, 66, 66, 255 }, { 255, 88, 88, 255 }, { 255, 125, 125, 255 }, { 255, 255, 255, 255 }, 12));
+	ui.text.push_back(textBox(renderer, "outtext", font, "OpenSans-ExtraBold.ttf", 250, 250, " ", { 255,255,255,255 }));
+	ui.inputs.push_back(textInput([&]() {getObjectById(ui.text, "outtext")->setText(getObjectById(ui.inputs, "inputtest")->typed); }, renderer, "inputtest", "test", font2, "OpenSans.ttf", 100, 100, 200, 40, 20, { 100, 100, 100, 100 }, { 60, 60, 60, 100 }, { 255, 255, 255, 255 }, 10.0, 7, false));
 
 	// Main Game Loop
 	while (gameloop) {
 		//Deltatime calc
 		LAST = NOW;
 		NOW = SDL_GetPerformanceCounter();
-		deltaTime = (double)((NOW - LAST) * 1000) / SDL_GetPerformanceFrequency(); 
+		deltaTime = (double)((NOW - LAST) * 1000) / SDL_GetPerformanceFrequency();
 
 		// Event Handler
-		handleEvents(gameloop, ui.buttons, renderer);
+		handleEvents(gameloop, ui, renderer);
 
 		// Clears The renderer
 		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
